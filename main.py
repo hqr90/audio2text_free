229d2639350737be1bda03a41609a2529fd6d2a6
backup <rebello.hiltonqueiroz@gmail.com>
2024-11-08 02:22:59 -0300

    202411080214

--- main.py
+++ main.py
@@ -8,6 +8,84 @@ from pydub import AudioSegment
 from moviepy.editor import VideoFileClip
 import yaml
 
+
+def convert_audio_to_wav(audio_file_path):
+    '''
+    Converte um arquivo de áudio para WAV.
+    :param audio_file_path:
+    :return:
+    '''
+    # Converte arquivos de áudio para WAV
+    audio = AudioSegment.from_file(audio_file_path)
+    temp_wav = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
+    audio.export(temp_wav.name, format='wav')
+    return temp_wav.name
+
+
+def extract_audio_from_video(video_file_path):
+    '''
+    Extrai o áudio de um arquivo de vídeo e salva como WAV.
+    :param video_file_path:
+    :return:
+    '''
+    # Extrai o áudio do vídeo e salva como arquivo WAV temporário
+    video = VideoFileClip(video_file_path)
+    temp_wav = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
+    video.audio.write_audiofile(temp_wav.name)
+    video.close()
+    return temp_wav.name
+
+
+def split_audio(file_path, chunk_length_ms=55000):
+    '''
+    Divide um arquivo de áudio em chunks de tamanho especificado.
+    :param file_path:
+    :param chunk_length_ms:
+    :return:
+    '''
+    # Carrega o áudio completo
+    audio = AudioSegment.from_wav(file_path)
+
+    # Lista para armazenar os caminhos dos chunks
+    chunks = []
+
+    # Divide o áudio em chunks
+    for i in range(0, len(audio), chunk_length_ms):
+        chunk = audio[i:i + chunk_length_ms]
+        temp_chunk = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
+        chunk.export(temp_chunk.name, format='wav')
+        chunks.append(temp_chunk.name)
+
+    return chunks
+
+
+def transcribe_audio(file_path, retries=3):
+    '''
+    Transcreve um arquivo de áudio usando a API do Google.
+    :param file_path:
+    :param retries:
+    :return:
+    '''
+    for attempt in range(retries):
+        try:
+            with sr.AudioFile(file_path) as source:
+                audio = recognizer.record(source)
+                # Reconhece a fala usando a API do Google
+                text = recognizer.recognize_google(audio, language="pt-BR")
+                time.sleep(1)  # Sleep de 1 segundo após transcrição bem-sucedida
+                return text
+        except Exception as e:
+            if attempt < retries - 1:
+                print(f"Ocorreu um erro: {e}. Tentando novamente em 20 segundos...")
+                time.sleep(20)  # Sleep de 20 segundos em caso de erro
+                continue
+            else:
+                print(f"Transcrição falhou após {retries} tentativas.")
+                return f"Ocorreu um erro: {e}"
+    # Se chegar aqui, todas as tentativas falharam
+    return "Transcrição falhou após múltiplas tentativas"
+
+
 def join_chunks(chunks, file_path):
     '''
     Junta os chunks em um único arquivo WAV e salva no caminho especificado.
@@ -22,6 +100,7 @@ def join_chunks(chunks, file_path):
         audio = audio + chunk
     audio.export(file_path, format='wav')
     return audio
+
 
 def process_file(file_path):
     '''
@@ -104,78 +183,6 @@ def process_file(file_path):
     print(full_transcription)
     return full_transcription
 
-def convert_audio_to_wav(audio_file_path):
-    '''
-    Converte um arquivo de áudio para WAV.
-    :param audio_file_path:
-    :return:
-    '''
-    # Converte arquivos de áudio para WAV
-    audio = AudioSegment.from_file(audio_file_path)
-    temp_wav = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
-    audio.export(temp_wav.name, format='wav')
-    return temp_wav.name
-
-def extract_audio_from_video(video_file_path):
-    '''
-    Extrai o áudio de um arquivo de vídeo e salva como WAV.
-    :param video_file_path:
-    :return:
-    '''
-    # Extrai o áudio do vídeo e salva como arquivo WAV temporário
-    video = VideoFileClip(video_file_path)
-    temp_wav = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
-    video.audio.write_audiofile(temp_wav.name)
-    video.close()
-    return temp_wav.name
-
-def split_audio(file_path, chunk_length_ms=55000):
-    '''
-    Divide um arquivo de áudio em chunks de tamanho especificado.
-    :param file_path:
-    :param chunk_length_ms:
-    :return:
-    '''
-    # Carrega o áudio completo
-    audio = AudioSegment.from_wav(file_path)
-
-    # Lista para armazenar os caminhos dos chunks
-    chunks = []
-
-    # Divide o áudio em chunks
-    for i in range(0, len(audio), chunk_length_ms):
-        chunk = audio[i:i + chunk_length_ms]
-        temp_chunk = tempfile.NamedTemporaryFile(delete=False, suffix='.wav')
-        chunk.export(temp_chunk.name, format='wav')
-        chunks.append(temp_chunk.name)
-
-    return chunks
-
-def transcribe_audio(file_path, retries=3):
-    '''
-    Transcreve um arquivo de áudio usando a API do Google.
-    :param file_path:
-    :param retries:
-    :return:
-    '''
-    for attempt in range(retries):
-        try:
-            with sr.AudioFile(file_path) as source:
-                audio = recognizer.record(source)
-                # Reconhece a fala usando a API do Google
-                text = recognizer.recognize_google(audio, language="pt-BR")
-                time.sleep(1)  # Sleep de 1 segundo após transcrição bem-sucedida
-                return text
-        except Exception as e:
-            if attempt < retries - 1:
-                print(f"Ocorreu um erro: {e}. Tentando novamente em 20 segundos...")
-                time.sleep(20)  # Sleep de 20 segundos em caso de erro
-                continue
-            else:
-                print(f"Transcrição falhou após {retries} tentativas.")
-                return f"Ocorreu um erro: {e}"
-    # Se chegar aqui, todas as tentativas falharam
-    return "Transcrição falhou após múltiplas tentativas"
 
 # Inicializa o reconhecedor
 recognizer = sr.Recognizer()
